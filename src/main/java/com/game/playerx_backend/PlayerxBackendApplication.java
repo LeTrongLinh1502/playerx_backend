4bbf51ad0a7ba439d1655154753c0be435d5f2da
JoyBoy <linhgoddragon@gmail.com>
2024-12-30 22:16:47 +0700

    push code base

---
+++ src/main/java/com/game/playerx_backend/PlayerxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.game.playerx_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerxBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/game/playerx_backend/PlayerxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.playerx_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
